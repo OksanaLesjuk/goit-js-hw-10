1e128a2f12451adb4ca43cc65af2ff53769a11f4
OksanaLesjuk <126483028+OksanaLesjuk@users.noreply.github.com>
2023-07-23 18:57:30 +0300

    markup

---
+++ src/js/cat-api.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const BASE_URL = 'https://api.thecatapi.com/v1';
+
+function fetchBreeds() {
+    return axios.get(`${BASE_URL}/breeds`)
+        .then(response => {
+            // повертаємо  дані з response.data( Axios автоматично розпізнає JSON-дані)
+            return response.data;
+        })
+        .catch(error => {
+
+            throw new Error("Помилка запиту:", error.message);
+        });
+
+}
+
+
+function fetchCatByBreed(breedId) {
+    return axios.get(`${BASE_URL}/images/search?breed_ids=${breedId}`)
+        .then(response => {
+            // повертаємо  дані з response.data( Axios автоматично розпізнає JSON-дані)
+            return response.data;
+        })
+        .catch(error => {
+
+            throw new Error("Помилка запиту:", error.message);
+        });
+
+}
+
+export { BASE_URL, fetchBreeds, fetchCatByBreed };
---
+++ src/js/index.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+import { BASE_URL, fetchBreeds, fetchCatByBreed } from "./cat-api"
+
+axios.defaults.headers.common["x-api-key"] = "live_C8fch3W7tmmF8fmpsXU2mdIfjeqC3R8Q3wAY9gs2QZiQFnTO5tNKg722MPHavC8b";
+
+const selectCat = document.querySelector(".breed-select")
+const catInfo = document.querySelector(".cat-info")
+selectCat.addEventListener("change", onSelectChange)
+
+
+
+function createCatList() {
+    fetchBreeds()
+        .then(data => {
+
+            const optionsList = data.map(({ id, name }) => ` <option value="${id}">${name}</option>`
+            ).join(' ');
+
+            selectCat.innerHTML = optionsList;
+
+        })
+        .catch(error => {
+
+            console.error("Помилка запиту:", error.message);
+        });
+}
+
+createCatList();
+
+
+
+function onSelectChange(evt) {
+    const selectedBreedId = evt.currentTarget.value;
+
+    fetchCatByBreed(selectedBreedId)
+        .then(data => {
+            const { breeds, url } = data[0];
+            const beerdCard = `<img class="pfoto-cat" width = "300px" src="${url}" alt="${breeds[0].name}">
+          <h2 class="name-cat">${breeds[0].name}</h2>
+          <p class="deskr-cat">Temperament:${breeds[0].description}</p>
+          <p class="temperament-cat">Temperament:${breeds[0].temperament}</p>`;
+
+            console.log(beerdCard);
+            catInfo.innerHTML = beerdCard;
+        })
+        .catch()
+}
